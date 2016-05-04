e3f20fc2891acdea6505112f2ff97575a07ddc46
Zhuinden <zhuinden@gmail.com>
2016-05-05 01:16:25 +0200

    added pull request for illegal state exception

--- flow/src/main/java/flow/Flow.java
+++ flow/src/main/java/flow/Flow.java
@@ -42,7 +42,12 @@ public final class Flow {
 
     @NonNull
     public static Flow get(@NonNull Context context) {
-        return InternalContextWrapper.getFlow(context);
+      Flow flow = InternalContextWrapper.getFlow(context);
+      if (null == flow) {
+        throw new IllegalStateException("Context was not wrapped with flow. "
+           + "Make sure attachBaseContext was overridden in your main activity");
+      }
+      return flow;
     }
 
     /**
