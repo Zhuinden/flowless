e47df1636a66e0aba294da0c33faa8c5b7851aa6
zhuinden <gabor.varadi@misoftware.hu>
2016-12-11 13:21:36 +0100

    ServiceProvider null guards

--- flowless-library/src/main/java/flowless/ServiceProvider.java
+++ flowless-library/src/main/java/flowless/ServiceProvider.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static flowless.Preconditions.checkNotNull;
+
 /**
  * Created by Zhuinden on 2016.08.29..
  */
@@ -15,20 +17,20 @@ public class ServiceProvider {
     public static class NoServiceException
             extends RuntimeException {
         private Object key;
-        private String tag;
+        private String serviceTag;
 
-        public NoServiceException(Object key, String tag) {
-            super("No service found for tag [" + tag + "] for key [" + key + "]");
+        public NoServiceException(Object key, String serviceTag) {
+            super("No service found for serviceTag [" + serviceTag + "] for key [" + key + "]");
             this.key = key;
-            this.tag = tag;
+            this.serviceTag = serviceTag;
         }
 
         public Object getKey() {
             return key;
         }
 
-        public String getTag() {
-            return tag;
+        public String getServiceTag() {
+            return serviceTag;
         }
     }
 
@@ -38,60 +40,67 @@ public class ServiceProvider {
         services = new LinkedHashMap<>();
     }
 
-    public <T> ServiceProvider bindService(View view, String tag, T service) {
-        return bindService(Flow.getKey(view), tag, service);
+    public <T> ServiceProvider bindService(View view, String serviceTag, T service) {
+        return bindService(Flow.getKey(view), serviceTag, service);
     }
 
-    public <T> ServiceProvider bindService(Context context, String tag, T service) {
-        return bindService(Flow.getKey(context), tag, service);
+    public <T> ServiceProvider bindService(Context context, String serviceTag, T service) {
+        return bindService(Flow.getKey(context), serviceTag, service);
     }
 
-    public <T> ServiceProvider bindService(Object key, String tag, T service) {
+    public <T> ServiceProvider bindService(Object key, String serviceTag, T service) {
+        checkNotNull(key, "key");
+        checkNotNull(serviceTag, "serviceTag");
+        checkNotNull(service, "service");
         if(!services.containsKey(key)) {
             services.put(key, new HashMap<String, Object>());
         }
         Map<String, Object> serviceMap = services.get(key);
-        serviceMap.put(tag, service);
+        serviceMap.put(serviceTag, service);
         return this;
     }
 
-    public boolean hasService(View view, String tag) {
-        return hasService(Flow.getKey(view), tag);
+    public boolean hasService(View view, String serviceTag) {
+        return hasService(Flow.getKey(view), serviceTag);
     }
 
-    public boolean hasService(Context context, String tag) {
-        return hasService(Flow.getKey(context), tag);
+    public boolean hasService(Context context, String serviceTag) {
+        return hasService(Flow.getKey(context), serviceTag);
     }
 
-    public boolean hasService(Object key, String tag) {
+    public boolean hasService(Object key, String serviceTag) {
+        checkNotNull(key, "key");
+        checkNotNull(serviceTag, "serviceTag");
         if(!services.containsKey(key)) {
             return false;
         }
         Map<String, Object> serviceMap = services.get(key);
-        return serviceMap.containsKey(tag);
+        return serviceMap.containsKey(serviceTag);
     }
 
     @NonNull
-    public <T> T getService(View view, String tag) {
-        return getService(Flow.getKey(view), tag);
+    public <T> T getService(View view, String serviceTag) {
+        return getService(Flow.getKey(view), serviceTag);
     }
 
     @NonNull
-    public <T> T getService(Context context, String tag) {
-        return getService(Flow.getKey(context), tag);
+    public <T> T getService(Context context, String serviceTag) {
+        return getService(Flow.getKey(context), serviceTag);
     }
 
     @NonNull
-    public <T> T getService(Object key, String tag) {
+    public <T> T getService(Object key, String serviceTag) {
+        checkNotNull(key, "key");
+        checkNotNull(serviceTag, "serviceTag");
         if(!services.containsKey(key)) {
-            throw new NoServiceException(key, tag);
+            throw new NoServiceException(key, serviceTag);
         }
         Map<String, Object> serviceMap = services.get(key);
-        if(serviceMap.containsKey(tag)) {
+        if(serviceMap.containsKey(serviceTag)) {
             //noinspection unchecked
-            return (T) serviceMap.get(tag);
+            return (T) serviceMap.get(serviceTag);
         } else {
-            throw new NoServiceException(key, tag);
+            throw new NoServiceException(key, serviceTag);
         }
     }
 
@@ -104,6 +113,7 @@ public class ServiceProvider {
     }
 
     public Map<String, Object> unbindServices(Object key) {
+        checkNotNull(key, "key");
         return services.remove(key);
     }
 }
