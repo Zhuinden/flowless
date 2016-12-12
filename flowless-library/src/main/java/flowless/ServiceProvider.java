851dccebc697b11ea8697dfd98d1755c4c4348b9
zhuinden <gabor.varadi@misoftware.hu>
2016-12-12 14:56:40 +0100

    add nullable

--- flowless-library/src/main/java/flowless/ServiceProvider.java
+++ flowless-library/src/main/java/flowless/ServiceProvider.java
@@ -2,6 +2,7 @@ package flowless;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 
 import java.util.HashMap;
@@ -104,14 +105,17 @@ public class ServiceProvider {
         }
     }
 
+    @Nullable
     public Map<String, Object> unbindServices(View view) {
         return unbindServices(Flow.getKey(view));
     }
 
+    @Nullable
     public Map<String, Object> unbindServices(Context context) {
         return unbindServices(Flow.getKey(context));
     }
 
+    @Nullable
     public Map<String, Object> unbindServices(Object key) {
         checkNotNull(key, "key");
         return services.remove(key);
